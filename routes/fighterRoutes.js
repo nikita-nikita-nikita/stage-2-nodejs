96b93dfc26373c88c7a793bca5299331fedd9a7b
Nikita <neymarenko123@gmail.com>
2020-05-16 17:18:15 +0300

    routes completed
    complete auth, fighter, user routes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -5,6 +5,54 @@ const { createFighterValid, updateFighterValid } = require('../middlewares/fight
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+router.get("/", (req, res, next) => {
+    const fighters = FighterService.getFighters();
+    if(fighters){
+        res.data = fighters;
+        next();
+    }else {
+        res.status(400).send({error:true, message:`can't find user with id`});
+    }
+}, responseMiddleware);
+
+
+router.post("/",(req, res, next) => {
+    if(req.body) {
+        res.data = {...req.body, id:req.params.id};
+        next();
+    }
+    res.status(404).send({error:true, message:"something got wrong"});
+}, createFighterValid, (req, res, next) => {
+    const fighter = FighterService.createNewFighter(res.data);
+    if (fighter){
+        next();
+    }
+},responseMiddleware);
+
+router.put("/:id", (req, res, next)=>{
+    if(req.body) {
+        res.data = {...req.body, id:req.params.id};
+        next();
+    }else {
+        res.status(404).send({error:true, message:"something got wrong"});
+    }
+}, updateFighterValid, (req, res, next)=>{
+    const user = FighterService.updateFighter(res.data.id, res.data);
+    if(user){
+        next();
+    }else {
+        res.status(400).send({error:true, message:`can't find fighter with id: ${req.params.id}`});
+    }
+},responseMiddleware);
+
+router.delete("/:id",(req,res,next)=>{
+    const fighters = FighterService.delete(req.params.id);
+    if(fighters.length===0){
+        res.status(400).send({error:true, message:`cannot delete fighter with id: ${req.params.id}`});
+    }else {
+        res.data = {fighters};
+        next();
+    }
+});
 
 module.exports = router;
